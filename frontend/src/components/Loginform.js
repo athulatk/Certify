9375e19db49d3f58f5646cee4ec410f100db7a97
N Athul Kumar <athulnvinod@gmail.com>
2021-06-05 17:07:02 +0530

    login-page

--- frontend/src/components/Loginform.js
+++ frontend/src/components/Loginform.js
@@ -1,30 +1,39 @@
 import React from 'react'
 import './Loginform.css'
-import {Button} from '@material-ui/core'
+import {Button,TextField} from '@material-ui/core'
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import VisibilityOffIcon from '@material-ui/icons/VisibilityOff';
 function Loginform() {
     return (
+        <div className="login__outerdiv">
+            <div className="login__side">
+            <h4>Welcome back!</h4>
+            </div>
         <div className="login__div">
-            <h2>Certify</h2>
+            <h3>Login</h3>
             <hr/>
             <h5>Welcome Back!</h5>
-            <h6>Please sign in to your account</h6>
+            <p style={{fontSize:'12px',marginBottom:'0.5em'}}>Please log in to your account</p>
             <form className="login__form">
                 <div className="login__inputs">
                 <div>
                 <label>UserId</label><br/>
-                <input type="text"/>
+                <input type="text" size="40" placeholder="Enter userid"/>
+                {/* <TextField id="outlined-basic" label="UserId" variant="outlined" /> */}
                 </div>
                 <div>
                 <label>Password</label><br/>
-                <input type="password"/>
+                <input type="password" size="40" placeholder="Enter password"/>
+                {/* <TextField id="outlined-basic" type="password" label="Password" variant="outlined"/> */}
+                {/* <VisibilityOffIcon/> */}
                 </div>
                 </div>
-                <hr/>
                 <div className="login__buttons">
-                <Button variant="contained" color="primary" style={{textTransform:'capitalize'}}>Recover Password</Button>
-                <Button variant="contained" color="primary" style={{textTransform:'capitalize'}}>Login</Button>
+                <Button style={{textTransform:'capitalize',backgroundColor:'white',color:'#4a86f7',border:'none'}}>Recover Password</Button>
+                <Button variant="contained" color="primary" style={{textTransform:'capitalize',backgroundColor:'#4a86f7'}}>Login</Button>
                 </div>
             </form>
+        </div>
         </div>
     )
 }
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Loginform from '../components/Loginform'
-import SideBanner from '../components/SideBanner'
+import './css/Loginpage.css'
+import Logo from '../assets/logo.svg'
+import Topcorner from '../assets/topcorner.svg'
+import Bottomcorner from '../assets/bottomcorner.svg'
+import Back from '../assets/background-obj.svg'
 function Login() {
     return (
-        <div>
-            <SideBanner/>
+        <div className="login_page">
+            <img src={Back} alt="" id="objects"/>
+            <img src={Topcorner} alt="" id="topcorner"/>
+            <img src={Bottomcorner} alt="" id="bottomcorner"/>
+            <div>
+            <img src={Logo} alt="" id="logo"/>
             <Loginform/>
+            </div>
         </div>
     )
 }
